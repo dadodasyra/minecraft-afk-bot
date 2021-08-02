37a3349346754c21e00f47826347e3f9a21de815
dadodasyra <44753923+dadodasyra@users.noreply.github.com>
2021-08-02 02:48:10 +0200

    Update bot.js
    Cleanup, add some commands and add sneakbydefault feature

--- bot.js
+++ bot.js
@@ -5,11 +5,11 @@ const readline = require('readline');
 
 const config = require('./config.json');
 
-var num = 0;
-var interval = null;
-var eating = false;
-var eatingslot
-var options = {
+let num = 0;
+let interval = null;
+let eating = false;
+let eatingslot;
+let options = {
 	host: config.server.address,
 	port: config.server.port,
 	username: config.user.username,
@@ -33,31 +33,16 @@ bot.on('chat', (username, message) => {
 	if (username === bot.username) return;
 
 	console.log(username + ": " + message);
-/* Désactivé les réponses automatique au message dans le chat, concernant par exemple pour dormir le bot peux deco ou répondre avec les tps
-	if(message == "sleep"){
-		bot.chat("Je me reco dans 5 secondes");
-		bot.quit();
-		process.exit();
-		return;
-	}
-
-	if(message == "afk"){
-		bot.chat("Je suis afk !");
-	}
-
-	if(message == "tps"){
-		bot.chat("Voici les tps incroyable : "+bot.getTps())
-	}*/
 });
 
 /*bot.on('playerJoined', (player) => { Tu peux l'activer si tu veux en supprimant le /* et * /
-	bot.chat("Hey " + player.username + "! Je suis un bot pas ouf, me déranger pas");
+	bot.chat("Hey " + player.username + "! Je suis un bot pas ouf, me dérangez pas");
 })*/
 
 bot.on('error', function(err) {
 	console.log('Erreur : ' + err.errno);
-	if (err.code == undefined) {
-		console.log('Mauvais identifiant, serveur inaccesible ou autre erreur non défini');
+	if (err.code === undefined) {
+		console.log('Mauvais identifiant, serveur inaccesible ou autre erreur non définie');
 	}
 	console.log('Reconnexion dans 60 secondes, Ctrl+C pour annuler');
 	process.exit(60)
@@ -73,6 +58,7 @@ bot.on('login', () => {
 
 bot.on('spawn', () => {
 	console.log("| Connecté")
+	if(config.toggle.sneakbydefault) bot.setControlState("sneak", true);
 });
 
 bot.on('health', () => {
@@ -90,72 +76,80 @@ bot.on('health', () => {
 		eating = true;
 		eatingslot = bot.quickBarSlot;
 		bot.setQuickBarSlot(config.toggle.autofeed.autofeedslot);
-		bot.consume(function(){bot.setQuickBarSlot(eatingslot);eating = false;});
+		bot.consume(function () {
+			bot.setQuickBarSlot(eatingslot);
+			eating = false;
+	 	});
 	}
 })
 
 rl.on("line", input =>{
-	array = input.split(' ')
-	if(input == "/quit"){
-		bot.quit();
-		process.exit(1);
+	let array = input.split(' ')
+	switch(array[0]){
+		case "/quit":
+			bot.quit();
+			process.exit(1);
+			break;
+		case "/reconnect":
+			bot.quit();
+			process.exit();
+			break;
+		case "/autoclick":
+			if(!array[1] || Number.isInteger(array[1])) array[1] = 700;
+
+			if(!interval){
+				interval = setInterval(autofunc, array[1]);
+				console.log("| Autoclick démarré")
+				return;
+			}
+			if(interval){
+				console.log("| Autoclick déjà actif, pour l'arrêter utilisez /stopautoclick")
+				return;
+			}
+			break;
+		case "/stopautoclick":
+			if(!interval){
+				console.log("| Il n'y a actuellement aucun autoclick d'actif");
+				return;
+			}
+			clearInterval(interval);
+			interval = null;
+
+			console.log("| Autoclick stoppé");
+			console.log("| Autoclicker a tapé "+num+" fois.");
+			num = 0;
+			break;
+		case "/tps":
+			console.log("| Les tps sont à "+bot.getTps());
+			break;
+		case "/sneak":
+			if(bot.getControlState("sneak")){
+				console.log("| Le bot ne sneak plus");
+				bot.setControlState("sneak", false);
+			} else {
+				console.log("| Le bot sneak maintenant");
+				bot.setControlState("sneak", true);
+			}
+			break;
+		case "/help":
+			console.log("/quit - Quitter");
+			console.log("/reconnect - Déco reconnect (fonctionne uniquement quand lancé via ./start.sh)");
+			console.log("/autoclick [ms] - Activer Autoclick sur entité la plus proche (par défault 700 ms | check entité plus proche + tape uniquement si c'est un mob)");
+			console.log("/stopautoclick - Stopper l'autoclick");
+			console.log("/sneak - Toggle sneak");
+			console.log("Contact : dadodasyra#0001 OU dadodasyra.pro@gmail.com")
+			break;
+		default:
+			bot.chat(input)
 	}
-
-	if(input == "/reconnect"){
-		bot.quit();
-		process.exit();
-	}
-
-	if(array[0] == "/autoclick"){
-		if(!array[1] || Number.isInteger(array[1])){
-			array[1] = 700;
-		}
-		if(!interval){
-			autoclick = null;
-			interval = setInterval(autofunc, array[1]);
-			console.log("| Autoclick started")
-			return;
-		}
-		if(interval){
-			console.log("| Autoclick déjà actif")
-			return;
-		}
-
-	}
-
-	if(input == "/stopautoclick"){
-		if(!interval){
-			console.log("| Il n'y a actuellement aucun autoclick d'actif");
-			return;
-		}
-		clearInterval(interval);
-		interval = null;
-
-
-		console.log("| Autoclick stopper");
-		console.log("| Autoclicker a tapé "+num+" fois.");
-		num = 0;
-		return;
-	}
-
-	if(input == "/help"){
-		console.log("/quit - Quitter");
-		console.log("/reconnect - Déco reconnect (fonctionne uniquement quand lancé via ./start.sh)");
-		console.log("/autoclick [ms] - Activer Autoclick sur entité la plus proche (par défault 700 ms | check entité plus proche + tape uniquement si c'est un mob)");
-		console.log("/stopautoclick - Stopper l'autoclick");
-		console.log("Contact : dadodasyra#0001 OU dadodasyra.pro@gmail.com")
-		return;
-	}
-
-	bot.chat(input)
 });
 
 function autofunc()
 {
-	entity = bot.nearestEntity()
+	let entity = bot.nearestEntity()
 	if(entity){
 		if (entity.type === 'mob') {
-			bot.attack(entity, true)
+			bot.attack(entity)
 			num++;
 		}
 	}
